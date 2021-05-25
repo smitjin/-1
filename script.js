c4aa73fc43e85a6aca970e41361d27daa8119301
giter <smit@gmail.com>
2021-05-25 16:07:01 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm('Are you here?');
+
+//console.log(result);
+
+//const answer = prompt("Вам есть 18 ?", "18");
+//console.log(answer+5);
+
+const a = [5, 7, 3, 8, 9, 'stroka' ]];
+
+fo (const i = 0; i<Array.length, i++) {console}
+//answers[0]= prompt('Как Ваше имя?',''); 
+//answers[1]= prompt('Как ваша фамилия?',''); 
+//answers[2]= prompt('Сколько Вам лет?',''); 
+
+const category = 'toys';
+alert(`Привет,category}`);
+
+let incr = 10,
+    decr = 10;
+
+    console.log(incr++ + ++decr);
+
+
+
+
